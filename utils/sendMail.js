8373ff3a0425fb310b4f935f551bf45aa2addacf
aatifnehal786 <aatifnehal786@gmail.com>
2025-10-02 17:06:18 +0530

    .

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,17 +1,12 @@
+import Resend from "resend";
 import dotenv from "dotenv";
 dotenv.config();
-import nodemailer from "nodemailer";
-import sgTransport from "nodemailer-sendgrid";
 
-const transporter = nodemailer.createTransport(
-  sgTransport({
-    apiKey: process.env.SENDGRID_API_KEY,
-  })
-);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOtpEmail = async (to, otp) => {
-  return transporter.sendMail({
-    from: `"Instagram Clone" <no-reply@aatifnehal786`, // must be verified in SendGrid
+  await resend.emails.send({
+    from: "Instagram Clone <no-reply@yourdomain.com>",
     to,
     subject: "Your OTP Code",
     text: `Your OTP code is ${otp}. It will expire in 10 minutes.`,
