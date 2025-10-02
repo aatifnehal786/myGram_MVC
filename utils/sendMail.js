cdb1ad6d44d64ab108ac7cdca6757649c6f9c747
aatifnehal786 <aatifnehal786@gmail.com>
2025-10-02 15:48:21 +0530

    .

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 import nodemailer from "nodemailer";
-import sgTransport from "nodemailer-sendgrid";
 
-const transporter = nodemailer.createTransport(
-  sgTransport({
-    apiKey: process.env.SENDGRID_API_KEY,
-  })
-);
+const transporter = nodemailer.createTransport({
+   service: "gmail",
+  auth: {
+    user: process.env.MY_GMAIL,          // e.g. your_email@gmail.com
+    pass: process.env.GMAIL_APP_PASS,    // 16-digit app password
+  },
+});
 
 export const sendOtpEmail = async (to, otp) => {
   return transporter.sendMail({
-    from: `"Instagram Clone" <no-reply@aatifnehal786>`, // verified domain sender
+    from: `"Instagram Clone" <${process.env.MY_GMAIL}>`,
     to,
     subject: "Your OTP Code",
     text: `Your OTP code is ${otp}. It will expire in 10 minutes.`,
