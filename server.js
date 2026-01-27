b8de5801ea8c2f59177afb40f5ff25e4ac81ec84
aatifnehal786 <aatifnehal786@gmail.com>
2026-01-27 17:42:26 +0530

    .

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ socketHandler(server);
 // Handling Cors
 
 app.use(cors({
-  origin: "https://mygram247.netlify.app" ,
+  origin: ["https://mygram247.netlify.app", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
