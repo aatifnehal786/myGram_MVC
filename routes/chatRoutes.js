b9aa699b49941f6dd486e77c7a49d69672bef293
aatifnehal786 <aatifnehal786@gmail.com>
2026-08-10 18:11:12 +0530

    .

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -21,8 +21,7 @@ router.post("/upload", auth,blockGuest, uploadChatFile);
 router.delete("/delete-chat", auth, blockGuest, deleteChatMessages);
 
 router.get("/chat/:userId",blockGuest, auth, getChat);
-// router.get("/search-users", auth, (req, res) => searchUsers(req, res, global.onlineUsers));
-router.get("/search-users",auth, (req, res) => searchUsers(req, res, global.onlineUsers));
+router.get("/search-users", auth, (req, res) => searchUsers(req, res, global.onlineUsers));
 router.get("/chat-list", auth, blockGuest, getChatList);
 router.post("/chat/forward", auth, blockGuest, forwardMessage);
 router.delete("/chat/deleteForMe/:messageId", auth, blockGuest, deleteForMe);
