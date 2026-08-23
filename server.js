edc6fca046730c5f567efaf0760b000e5cbf9dd2
aatifnehal786 <aatifnehal786@gmail.com>
2026-08-23 16:09:23 +0530

    .

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors({
 // Create limiter
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 30, // Limit each IP to 30 requests per windowMs
+  max: 20, // Limit each IP to 20 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
